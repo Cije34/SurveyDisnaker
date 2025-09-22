e4873b678af374548303773c3dea177112c2332d
ahmad ali reza <pasanjal34@gmail.com>
2025-09-22 12:41:37 +0700

    migrasi:membuat model, migrasi dan seeder TAHUN KEGIATAN DAN KEGIATAN

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            TahunKegiatanSeeder::class,
+            KegiatanSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role' => 'admin',
+            'password' => bcrypt('password'), // Ensure to hash the password
         ]);
     }
 }
