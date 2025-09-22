7747cf4dd5c4f1a2c5a1be44d94f40318f5e5bba
ahmad ali reza <pasanjal34@gmail.com>
2025-09-22 14:05:08 +0700

    migrasi:membuat model, migrasi dan seeder Survey

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             MentorSeeder::class,
             TempatSeeder::class,
             PesertaSeeder::class,
+            SurveySeeder::class,
         ]);
 
         User::factory()->create([
