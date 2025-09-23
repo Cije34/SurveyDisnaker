ec32f557d418ce0e8e651f510d15d6b2e06d9b93
rhpz <zphr@tuta.io>
2025-09-23 10:22:11 +0700

    static user dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', 'Disnaker')</title>
-  @vite(['resources/css/dashboard.css'])
+  <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 </head>
 <body>
   <div class="container">
